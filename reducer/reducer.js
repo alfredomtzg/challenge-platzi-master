7cd3bf5b90b3fd05ae0419ea9b2d17af22c3bd3c
Alfredo Martinez <psialfredomtz@gmail.com>
2020-10-04 20:17:13 -0500

    Validar

--- reducer/reducer.js
+++ reducer/reducer.js
@@ -1,4 +1,4 @@
-const user = {
+const user1 = {
   "_id": "5f73591e51d3390017a1a693",
   "username": "Carlos",
   "roles": [
@@ -16,6 +16,25 @@ const user = {
   "message": "Auth Succesful",
   "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmNzM1OTFlNTFkMzM5MDAxN2ExYTY5MyIsImlhdCI6MTYwMTg1NTk2MywiZXhwIjoxNjAxOTQyMzYzfQ.GXRoGJwF8cY2jhmdgin0P_SHBcOdrFYMKQGU3wFtvho"
 }
+const user2 = {
+  "_id": "5f73591e51d3390017a1a693",
+  "username": "Carlos",
+  "roles": [
+      {
+          "_id": "5f653764b88fc70017211ed3",
+          "name": "user",
+          "__v": 0
+      },
+  ],
+  "message": "Auth Succesful",
+  "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmNzM1OTFlNTFkMzM5MDAxN2ExYTY5MyIsImlhdCI6MTYwMTg1NTk2MywiZXhwIjoxNjAxOTQyMzYzfQ.GXRoGJwF8cY2jhmdgin0P_SHBcOdrFYMKQGU3wFtvho"
+}
 
 
-console.log(user.roles.map(role => role.name == "admin"));
+
+
+console.log(user1.roles.length);
+
+console.log(user2.roles.length);
+user1.roles.length === 2 ? console.log('yes'): console.log('no');
+user2.roles.length === 2 ? console.log('yes'): console.log('no');
